b6837fb44058b5e4bbdc1eb9c7648dab0e7d34dc
Christopher Danan <chrisddan@gmail.com>
2015-05-03 22:38:39 -0700

    Add information paragraphs that will output details to the user. Keep track of the number of "ship pegs" the user has left. NOTE: We are not yet checking for valid placement of pegs.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,13 +3,32 @@ var main = function(){
 
 	console.log("VANE!@@@@!@!@!@@!@");
 
-	$("#grid").click(function(cell){
-		var $clickedCell = $(cell.target).attr("id");
+	var numShipPegs = 5;  //Number of pegs user has to place on the board.
+	var $clickInfo = $("<p>").addClass("clickInfo").text("You clicked: ");
+	var $instructions = $("<p>").addClass("instructions").text("Place pegs on the board to represent your ships");
+	var $numShipsLeft = $("<p>").addClass("numShipsLeft").text("Number of ships left: " + numShipPegs);
+
+	$("#clicked").append($instructions);
+	$("#clicked").append($clickInfo);
+	$("#clicked").append($numShipsLeft);
+
+	//Handle when a table cell is clicked.
+	$("#grid td").click(function(cell){
+		var $clickedCell = $(cell.target).attr("id");  //Get the id of the cell that was clicked.
 
 		console.log($clickedCell);
 
-		$("#clicked p").text("You clicked: " + $clickedCell);
+		//Output to the user what he/she clicked.
+		$("#clicked .clickInfo").text("You clicked: " + $clickedCell);
+
+		if(confirm("Are you sure you want to place a peg here?")){
+			$(cell.target).addClass("ship");
+			numShipPegs--;
+			$numShipsLeft.text("Number of ships left: " + numShipPegs);
+		}
 	});
+
+
 };
 
 $(document).ready(main);
