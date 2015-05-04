bfdc8bc80eb3ff4eb4fac5d92732ac6a5baae488
Christopher Danan <chrisddan@gmail.com>
2015-05-03 23:19:59 -0700

    Add a json object to keep track of ships and their information (such as number of pegs for each ship).

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,14 +3,30 @@ var main = function(){
 
 	console.log("VANE!@@@@!@!@!@@!@");
 
-	var numShipPegs = 5;  //Number of pegs user has to place on the board.
+	//var numShipPegs = 5;  //Number of pegs user has to place on the board.
+	var ships = [
+					{"name": "battleship", "numPegs": 4},
+					{"name": "cruiser", "numPegs": 3},
+					{"name": "submarine", "numPegs": 2}
+				];
+
 	var $clickInfo = $("<p>").addClass("clickInfo").text("You clicked: ");
 	var $instructions = $("<p>").addClass("instructions").text("Place pegs on the board to represent your ships");
+	var $subInstructions = $("<p>").addClass("placeShipInstructions");
 	var $numShipsLeft = $("<p>").addClass("numShipsLeft").text("Number of ships left: " + numShipPegs);
 
 	$("#clicked").append($instructions);
+	$("#clicked").append($subInstructions);
 	$("#clicked").append($clickInfo);
-	$("#clicked").append($numShipsLeft);
+	//$("#clicked").append($numShipsLeft);
+
+	//console.log(ships[0].name + " has " + ships[0].numPegs + " pegs.");
+
+	ships.forEach(function(ship){
+		var numShipPegs = ship.numPegs;
+		
+		$subInstructions.text("Place " + ship.numPegs + " pegs for " + ship.name);
+	});
 
 	//Handle when a table cell is clicked.
 	$("#grid td").click(function(cell){
@@ -21,11 +37,12 @@ var main = function(){
 		//Output to the user what he/she clicked.
 		$("#clicked .clickInfo").text("You clicked: " + $clickedCell);
 
+		/*
 		if(confirm("Are you sure you want to place a peg here?")){
 			$(cell.target).addClass("ship");
 			numShipPegs--;
 			$numShipsLeft.text("Number of ships left: " + numShipPegs);
-		}
+		}*/
 	});
 
 
