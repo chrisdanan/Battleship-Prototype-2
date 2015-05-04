1f14ec8015b84ae7cf4783cb65a5fcc167c6be48
Christopher Danan <chrisddan@gmail.com>
2015-05-04 11:39:01 -0700

    Add a list of ships that will be outputted to the user. Each ship on the list is clickable and will be used for the placement of ships on the grid.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,27 +5,50 @@ var main = function(){
 
 	//var numShipPegs = 5;  //Number of pegs user has to place on the board.
 	var ships = [
-					{"name": "battleship", "numPegs": 4},
-					{"name": "cruiser", "numPegs": 3},
-					{"name": "submarine", "numPegs": 2}
+					{"name": "battleship", "numPegs": 4, "set": "unset"},
+					{"name": "cruiser", "numPegs": 3, "set": "unset"},
+					{"name": "submarine", "numPegs": 2, "set": "unset"}
 				];
 
 	var $clickInfo = $("<p>").addClass("clickInfo").text("You clicked: ");
 	var $instructions = $("<p>").addClass("instructions").text("Place pegs on the board to represent your ships");
 	var $subInstructions = $("<p>").addClass("placeShipInstructions");
-	var $numShipsLeft = $("<p>").addClass("numShipsLeft").text("Number of ships left: " + numShipPegs);
+	var $hoverInfo = $("<p>").addClass("hoverInfo").text("Hovering over: ");
+	//var $numShipsLeft = $("<p>").addClass("numShipsLeft").text("Number of ships left: " + numShipPegs);
 
 	$("#clicked").append($instructions);
 	$("#clicked").append($subInstructions);
+	$("#clicked").append($hoverInfo);
 	$("#clicked").append($clickInfo);
 	//$("#clicked").append($numShipsLeft);
 
 	//console.log(ships[0].name + " has " + ships[0].numPegs + " pegs.");
 
+	/*
 	ships.forEach(function(ship){
 		var numShipPegs = ship.numPegs;
 		
 		$subInstructions.text("Place " + ship.numPegs + " pegs for " + ship.name);
+	});*/
+
+	//Place ships on a list and handle click events for each ship.
+	ships.forEach(function(ship){
+		//First, create the list of ships to be outputted to the user.
+		var $ul = $("<ul>"),
+			$li = $("<li>").attr("id", ship.name).addClass("ship").text(ship.name);
+
+		$ul.append($li);
+		$("#shipList").append($ul);
+
+		$li.on("click", function(){
+			console.log($li.attr("id"));
+
+			//Next, remove "activeShip" class from list.
+			$("#shipList li").removeClass("activeShip");
+
+			//Give clicked ship "activeShip" class.
+			$li.addClass("activeShip");
+		})
 	});
 
 	//Handle when a table cell is clicked.
@@ -45,6 +68,10 @@ var main = function(){
 		}*/
 	});
 
+	$("#grid td").mouseover(function(cell){
+		//console.log(cell.target.id);
+		$hoverInfo.text("Hovering over: " + cell.target.id);
+	});
 
 };
 
