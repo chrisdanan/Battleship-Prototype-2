f0acb107d1538eaff732350366d6ceda89b3c41a
Christopher Danan <chrisddan@gmail.com>
2015-05-03 21:38:48 -0700

    Add code to catch which cell was clicked.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,14 @@ var main = function(){
 	"use strict";
 
 	console.log("VANE!@@@@!@!@!@@!@");
+
+	$("#grid").click(function(cell){
+		var $clickedCell = $(cell.target).attr("id");
+
+		console.log($clickedCell);
+
+		$("#clicked p").text("You clicked: " + $clickedCell);
+	});
 };
 
 $(document).ready(main);
